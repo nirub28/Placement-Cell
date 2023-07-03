93934b080cd79b2117f752a43484a333e00b01f0
Niranjan P B <niranjanbgowda123@gmail.com>
2023-07-03 21:51:53 +0530

    views and layouts

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express= require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(express.static('./assets')); // to use asset folders
+
+//use layouts, should be before routes
+app.use(expressLayouts);
+
+// take all css/script files in subpages and put it in layout header, else it will in body tag
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+//use express routes
+app.use('/', require('./routes'));
+
+//set view engine
+app.set('view engine','ejs');
+app.set('views', './views');
+
 
 app.listen(port, function(err){
     if(err){
