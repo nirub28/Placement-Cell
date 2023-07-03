02e33bc5e05aeb2e562cdaeb538d544517e94921
Niranjan P B <niranjanbgowda123@gmail.com>
2023-07-03 23:00:24 +0530

    Intalled mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://localhost/placement_development');
+
+const db=mongoose.connection;
+
+db.on('error', console.error.bind('Error connecting to MongoDB'));
+
+db.once('open' , function(){
+    console.log('Conneted to Database:: MongoDB');
+})
+
+module.exports=db;
