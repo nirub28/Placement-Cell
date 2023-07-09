3a735e18e169d2f725457e4132be9ac142b169e9
Niranjan P B <niranjanbgowda123@gmail.com>
2023-07-09 23:20:53 +0530

    Updated results in profile

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -5,17 +5,34 @@ const Interview = require('../models/Interview');
 //student profile
 module.exports.profile = async function(req, res) {
   try {
-    const student = await Student.findById(req.params.id)
-    .populate("interviews.interview", "company date result");
+    const student = await Student.findById(req.params.id).populate('interviews.interview');
+      if (student) {
+        const interviews = await Promise.all(student.interviews.map(async interview => {
+          const interviewData = await Interview.findById(interview.interview);
+          const result = interviewData ? interview.result : 'N/A'; // Extract the result if interviewData exists, otherwise set as 'N/A'
+  
+          return {
+            ...interviewData.toObject(),
+            result: result
+          };
+        }));
 
-    return res.render('student_profile', {
-      title: "Student Profile",
-      profile_student: student
-    });
+
+      return res.render('student_profile', {
+        title: "Student Profile",
+        profile_student: student,
+        interviews: interviews
+      });
+    } else {
+      return res.status(404).send("Student not found");
+    }
   } catch (err) { 
     console.log('Error in finding student:', err);
+    return res.status(500).send("Internal Server Error");
   }
 };
+
+
 
 module.exports.add = async function (req, res) {
   try {
