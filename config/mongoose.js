f367aad5f65a0af6f6864b0cbb2a1e892939368e
Niranjan P B <niranjanbgowda123@gmail.com>
2023-07-12 01:11:38 +0530

    Added cloud DB

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");  // data base connection
 
-mongoose.connect("mongodb://127.0.0.1:27017/placement_development");
+// mongoose.connect("mongodb://127.0.0.1:27017/placement_development");
+mongoose.connect("mongodb+srv://nirub:nirub283@cluster0.ye8q8b0.mongodb.net/cdn?retryWrites=true&w=majority");
+
 
 const db = mongoose.connection;
 
